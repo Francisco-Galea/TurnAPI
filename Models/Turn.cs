bb3708b38ee666420f92fc3217c104a176ef0c00
Francisco Galea <galeafrancisco2001@gmail.com>
2025-12-16 22:08:51 -0300

    Added class Turn

---
+++ Models/Turn.cs
@@ -0,0 +1,14 @@
+﻿namespace TurnApi.Models
+{
+    public sealed class Turn
+    {
+        public int turnId { get; set; }
+        public int agendaId { get; set; }
+        public int clientId { get; set; }
+        public int employeeId { get; set; }
+        public DateTime turnStart { get; set; }
+        public DateTime turnEnd { get; set; }
+        public string status { get; set; }
+        public string notes { get; set; }
+    }
+}
