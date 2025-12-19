7a1be2a438b274246c4473592450de2438f0c91d
Francisco Galea <galeafrancisco2001@gmail.com>
2025-12-19 20:17:21 -0300

    Deleted the userSecrets reference [TEMPORAL

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using TurnApi.DTOs.Request;
+using TurnApi.Repositories.Interface;
+
+namespace TurnApi.Repositories
+{
+    internal sealed class AccountRepository : IAccountRepository
+    {
+
+        public void CreateAccount(AccountCreationRequest accountRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
