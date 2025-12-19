27b5b0f8ad355db8a0f50a9cb3205553c7224362
Francisco Galea <galeafrancisco2001@gmail.com>
2025-12-19 20:18:21 -0300

    Fixed DI implementation

---
+++ Services/AccountService.cs
@@ -0,0 +1,37 @@
+﻿using TurnApi.DTOs.Request;
+using TurnApi.Repositories.Interface;
+using TurnApi.Services.Interfaces;
+
+namespace TurnApi.Services
+{
+    internal sealed class AccountService : IAccountService
+    {
+
+        private readonly IAccountRepository accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+        
+
+        public void CreateAccount(AccountCreationRequest accountRequest)
+        {
+            try
+            {
+                accountRepository.CreateAccount(accountRequest);
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+
+
+    }
+}
