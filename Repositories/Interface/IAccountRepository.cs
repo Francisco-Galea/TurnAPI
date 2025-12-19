b226bf95344e4a4f8c2cd9c6df76b1e3cccd2fb7
Francisco Galea <galeafrancisco2001@gmail.com>
2025-12-19 20:17:56 -0300

    Changed class access modifier from internal to public

---
+++ Repositories/Interface/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using TurnApi.DTOs.Request;
+
+namespace TurnApi.Repositories.Interface
+{
+    public interface IAccountRepository
+    {
+        void CreateAccount(AccountCreationRequest accountRequest);
+    }
+}
