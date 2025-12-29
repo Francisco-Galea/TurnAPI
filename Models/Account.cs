56bb1deafd3201eae61aa8b46b4beb30d1a97361
Francisco <galeafrancisco2001@gmail.com>
2025-12-29 11:05:57 -0300

    Added more attributtes

--- Models/Account.cs
+++ Models/Account.cs
@@ -3,6 +3,10 @@
     public class Account
     {
         public int accountId {  get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public int document {  get; set; }
+        public int phoneNumber { get; set; }
         public string email { get; set; }
         public string password { get; set; }
 
