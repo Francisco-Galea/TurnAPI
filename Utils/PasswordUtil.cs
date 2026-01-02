184c14aaa77ea7e52f290f6eb90bbca99e235da4
Francisco <galeafrancisco2001@gmail.com>
2026-01-02 10:53:00 -0300

    Implemented HashPassword and ValidatePassword methods

---
+++ Utils/PasswordUtil.cs
@@ -0,0 +1,16 @@
+﻿namespace TurnApi.Utils
+{
+    internal static class PasswordUtil
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool IsPasswordValid(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+
+    }
+}
