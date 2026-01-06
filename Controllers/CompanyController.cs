dbc325dead636340a0663980acb072f2856997f0
Francisco <galeafrancisco2001@gmail.com>
2026-01-06 14:43:10 -0300

    Implementing endpoints

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TurnApi.DTOs.Request;
+using TurnApi.Services.Interfaces;
+
+namespace TurnApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+
+        private readonly ICompanyService companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+
+        [HttpPost]
+        public void CreateCompany([FromBody] CreateCompanyRequest createCompanyRequest)
+        {
+            companyService.CreateCompany(createCompanyRequest);
+        }
+
+    }
+}
