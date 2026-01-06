c83182b4b3de6f5a1fa371d66c063ccf4cdd3fba
Francisco <galeafrancisco2001@gmail.com>
2026-01-06 14:44:59 -0300

    Implemented CompanyService

---
+++ Services/CompanyService.cs
@@ -0,0 +1,31 @@
+﻿using TurnApi.DTOs.Request;
+using TurnApi.Models;
+using TurnApi.Repositories.Interface;
+using TurnApi.Services.Interfaces;
+
+namespace TurnApi.Services
+{
+    public class CompanyService : ICompanyService
+    {
+
+        private ICompanyRepository companyRepository;
+
+        public CompanyService(ICompanyRepository companyRepository)
+        {
+            this.companyRepository = companyRepository;
+        }
+
+        public void CreateCompany(CreateCompanyRequest createCompanyRequest)
+        {
+            Company company = new Company()
+            {
+                companyName = createCompanyRequest.companyName,
+                accountId = createCompanyRequest.founderAccountId,
+                socialReason = createCompanyRequest.socialReason,
+                cuit = createCompanyRequest.cuit
+            };
+            companyRepository.CreateCompanyRequest(createCompanyRequest);
+        }
+
+    }
+}
