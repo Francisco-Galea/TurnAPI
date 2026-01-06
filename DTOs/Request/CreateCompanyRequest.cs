fbdd7e9e638ad41d0ac4376a7ef4e0b82963d77d
Francisco <galeafrancisco2001@gmail.com>
2026-01-06 14:43:36 -0300

    Implemented Create company Request

---
+++ DTOs/Request/CreateCompanyRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TurnApi.DTOs.Request
+{
+    public sealed class CreateCompanyRequest
+    {
+        public int founderAccountId { get; set; }
+        public string companyName { get; set; }
+        public string socialReason { get; set; }
+        public string cuit {  get; set; }
+    }
+}
