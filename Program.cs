3db81e73fcd8d13ed967180586683084d8c4c39b
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-09 08:58:55 -0300

    Minimal changes

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionSql = builder.Configuration["ConnectionString:SQLSERVER"];
+var connectionSql = builder.Configuration["ConnectionStrings:SQLSERVER"];
 
 var app = builder.Build();
 
