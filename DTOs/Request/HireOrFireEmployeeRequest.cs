a627259865874d69ec755e20e6ccee8ab32f9a4b
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-10 12:15:24 -0300

    Implemented request to Hire and Fire an Employee

---
+++ DTOs/Request/HireOrFireEmployeeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TurnApi.DTOs.Request
+{
+    public class HireOrFireEmployeeRequest
+    {
+        public int companyId { get; set; }
+        public int accountId { get; set; }
+    }
+}
