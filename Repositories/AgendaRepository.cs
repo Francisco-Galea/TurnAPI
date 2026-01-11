cc27513ad3555da6017e19ad7a2e28f3c9fc08a9
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-11 19:54:58 -0300

    Implemented CreateAgenda method

---
+++ Repositories/AgendaRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using TurnApi.DTOs.Request;
+using TurnApi.Repositories.Interface;
+
+namespace TurnApi.Repositories
+{
+    public class AgendaRepository : IAgendaRepository
+    {
+
+        private IConfiguration configuration { get; }
+        private string connectionString { get; }
+
+        public AgendaRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.connectionString = this.configuration["ConnectionStrings:SQLSERVER"];
+        }
+
+        public void CreateAgenda(CreateAgendaRequest createAgendaRequest)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var query = "INSERT INTO Agendas (CompanyId, Name, Description, TurnDurationInMinutes) " +
+                        "VALUES (@CompanyId, @Name, @Description, @TurnDurationInMinutes);";
+            connection.Query(query, new
+            {
+                CompanyId = createAgendaRequest.companyId,
+                Name = createAgendaRequest.name,
+                Description = createAgendaRequest.description,
+                TurnDurationInMinutes = createAgendaRequest.turnDurationInMinutes
+            });
+        }
+    }
+}
---
+++ Repositories/Interface/IAgendaRepository.cs
@@ -0,0 +1,9 @@
+﻿using TurnApi.DTOs.Request;
+
+namespace TurnApi.Repositories.Interface
+{
+    public interface IAgendaRepository
+    {
+        void CreateAgenda(CreateAgendaRequest createAgendaRequest);
+    }
+}
