5a6972da74b8a2e2779ef2b43927277f1c583958
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-11 19:39:03 -0300

    Implemented CreateAgendaRequest

---
+++ DTOs/Request/CreateAgendaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TurnApi.DTOs.Request
+{
+    public class CreateAgendaRequest
+    {
+        public int companyId {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int turnDurationInMinutes { get; set; }
+    }
+}
