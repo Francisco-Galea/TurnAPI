4fa1e59af290d620a6478ed104314946acfe073c
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-13 20:02:24 -0300

    Added new attributtes employeeId, pisitionInCompany and descriptionOfPosition

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -2,6 +2,9 @@
 {
     internal sealed class Employee
     {
+        public int employeeId { get; set; }
+        public string positionInCompany {  get; set; }
+        public string descriptionOfPosition {  get; set; }
         public int accountId {  get; set; }
         public int companyId { get; set; }
     }
