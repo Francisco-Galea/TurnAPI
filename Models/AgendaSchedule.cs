6d60fb0a2ee4c9802ec57fb27e2415f01f51d70c
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-13 20:01:47 -0300

    Changed TurnInit and TurnEnd from DateTime to TimeOnly

--- Models/AgendaSchedule.cs
+++ Models/AgendaSchedule.cs
@@ -5,7 +5,7 @@
         public int agendaScheduleId {  get; set; }
         public int agendaId { get; set; }
         public string workableDay { get; set; }
-        public DateTime turnInit {  get; set; }
-        public DateTime turnEnd { get; set; }
+        public TimeOnly turnInit {  get; set; }
+        public TimeOnly turnEnd { get; set; }
     }
 }
