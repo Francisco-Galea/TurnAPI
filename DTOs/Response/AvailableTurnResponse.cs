dbd58eb050aa1b33a737cd75eb85a470f9ff2036
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-21 08:35:35 -0300

    Added agenda response

---
+++ DTOs/Response/AvailableTurnResponse.cs
@@ -0,0 +1,10 @@
+﻿using TurnApi.Models.Enums;
+
+namespace TurnApi.DTOs.Response
+{
+    public class AvailableTurnResponse
+    {
+        public TimeOnly turnInit {  get; set; }
+        public TimeOnly turnEnd { get; set; }
+    }
+}
