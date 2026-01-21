951cbaeae857ee3830d1e1cb6318460dfd4ceb7b
Francisco Galea <galeafrancisco2001@gmail.com>
2026-01-21 08:35:19 -0300

    Added createTurnRequest

---
+++ DTOs/Request/CreateTurnRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TurnApi.DTOs.Request
+{
+    public class CreateTurnRequest
+    {
+        public int agendaId { get; set; }
+        public int accountClientId { get; set; }
+        public DateOnly turnDate {  get; set; }
+        public TimeOnly turnInit { get; set; }
+        public string notes { get; set; }
+    }
+}
